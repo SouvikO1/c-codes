f53086d6e35c300abb421229d17b13f6d6267ad9
Souvik Mondal <144446948+SouvikO1@users.noreply.github.com>
2023-10-03 22:53:34 +0530

    Add files via upload

---
+++ basic_c_codes/3.print the area of a square.c
@@ -0,0 +1,13 @@
+//print the area of a square(3)
+#include <stdio.h>
+
+int main()
+{
+    float side;
+    printf("enter the values of the side:");
+    scanf("%f",&side);
+    printf("the area of the square:%f",(side*side));
+
+    return 0;
+}
+
