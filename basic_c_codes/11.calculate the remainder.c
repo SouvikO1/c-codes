a46168ec179311792b06734d8e765ff83d330567
Souvik Mondal <144446948+SouvikO1@users.noreply.github.com>
2023-10-04 21:40:57 +0530

    Add files via upload

---
+++ basic_c_codes/11.calculate the remainder.c
@@ -0,0 +1,13 @@
+//calculate the remainder(11)
+
+#include<stdio.h>
+int main(){
+	int num1,num2;
+	printf("enter first number:");
+	scanf("%d",&num1);
+	printf("enter second number:");
+	scanf("%d",&num2);
+	printf("remainder:%d",(num1%num2));
+	
+	return 0;
+}
