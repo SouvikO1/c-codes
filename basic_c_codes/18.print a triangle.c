a9476ec9f095ef3d6fb2174a56f2a254d9f9e590
Souvik Mondal <144446948+SouvikO1@users.noreply.github.com>
2023-10-04 21:42:53 +0530

    Add files via upload

---
+++ basic_c_codes/18.print a triangle.c
@@ -0,0 +1,11 @@
+//print a triangle(18)
+
+#include<stdio.h>
+int main(){
+	printf("      *\n");
+	printf("     * *\n");
+	printf("    * * *\n");
+	printf("   * * * *\n");
+	printf("  * * * * *\n");
+	return 0;
+}
