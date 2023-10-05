85d7801448653214e00298575a6914db3bbe7bc8
Souvik Mondal <144446948+SouvikO1@users.noreply.github.com>
2023-10-05 23:19:42 +0530

    Add files via upload

---
+++ conditionals_c/3.check if a student is pass or fail.c
@@ -0,0 +1,16 @@
+//check if a student is pass or fail(3)
+#include<stdio.h>
+int main(){
+	int n;
+	printf("enter the marks:");
+	scanf("%d",&n);
+	if(n>=40){
+		printf("PASS");
+	}
+	else{
+		printf("FAIL");
+	}
+	
+	
+	return 0;
+}
