4a629a4e51685d5b998bb9c0bc594f623a1738f0
Souvik Mondal <144446948+SouvikO1@users.noreply.github.com>
2023-10-06 23:36:07 +0530

    Add files via upload

---
+++ conditionals_c/16.print the number from 1-n.c
@@ -0,0 +1,12 @@
+//print the number from 1-n(16)
+#include<stdio.h>
+int main(){
+	int n;
+	printf("enter the number upto which you want to print:");
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+	
+		printf("%d\n",i);
+	}
+	return 0;
+}
