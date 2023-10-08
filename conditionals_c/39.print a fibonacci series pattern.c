8ffca4b474fab8e922906b790a20cdfd9645fdfb
Souvik Mondal <144446948+SouvikO1@users.noreply.github.com>
2023-10-08 21:28:02 +0530

    Add files via upload

---
+++ conditionals_c/39.print a fibonacci series pattern.c
@@ -0,0 +1,25 @@
+// print a fibonacci series pattern (39)
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the number of rows you want to print:");
+    scanf("%d",&n);
+    int count=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d ",count);
+            count++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
